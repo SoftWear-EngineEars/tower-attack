a9137f7c2fd0e61e24fb25843244d6b585509f6c
Pinak Unawane <pinuna27@bergen.org>
2025-11-20 12:07:02 -0500

    Add singleton functionality and controlled gold operations

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -2,6 +2,24 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
+
+    public static ResourceManager Instance { get; private set; }
+
+    public int Gold { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +30,23 @@ public class ResourceManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GainGold(int amount)
+    {
+        Gold += amount;
+    }
+
+    public bool SpendGold(int amount)
+    {
+        if (Gold >= amount)
+        {
+            Gold -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
