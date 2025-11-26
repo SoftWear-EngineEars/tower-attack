e16338e9de22d77ac2155bbd0b338cc078347efc
MiklPolygon <154548611+MiklPolygon@users.noreply.github.com>
2025-11-26 17:50:14 -0500

    monsters spawn on click when toggled
    ...and when you have enough money

--- Assets/Scripts/ScriptableObjects/MonsterData.cs
+++ Assets/Scripts/ScriptableObjects/MonsterData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Monster", menuName = "Scriptable Objects/Monster")]
 public class MonsterData : EntityData
 {
+
+    public int cost;
     public Sprite LoadSprite()
     {
         return LoadSprite("Monster");
