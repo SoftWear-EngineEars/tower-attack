c0b7073d59b4dde34cc53059561282be6c634d7e
JJ <jjl2008.com@gmail.com>
2025-11-30 22:04:21 -0500

    Renamed GameOverButtons to reuse in other parts of the game

---
+++ Assets/Scripts/GameStartingButtons.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class GameOverButtons : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void QuitButton()
+    {
+        Application.Quit(); // quitting in built version
+        EditorApplication.isPlaying = false; // quitting when testing in editor version
+    }
+}
