36a36cf1fd5417f1852685e2eba998d52dd93626
Korn-Jacob <jackor27@bergen.org>
2025-12-01 07:32:44 -0500

    monsters die die when hitting xvan

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -83,6 +83,8 @@ public class Monster : MonoBehaviour
             if (IsTouchingTower())
             {
                 _targetTower.GetComponent<HealthSystem>()?.TakeDamage(_monsterData.damage);
+                if (_targetTower.GetComponent<Tower>().IsCenterTower()) 
+                    Destroy(gameObject);
             }
             yield return new WaitForSeconds(damageDelay);
         }
